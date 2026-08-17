a71f50e297a5d2520b74537b9e84cd68ccae8f89
santiagomi0317-netizen <santiagomi0317@gmail.com>
2026-08-17 14:04:29 -0500

    Archivo de soporte de Ejercicios uno y dos

---
+++ LAB01/Ejercicio 1 y 2.pl
@@ -0,0 +1,23 @@
+% Hechos: relación entre ciudades
+ciudad(bogota).
+ciudad(medellin).
+ciudad(cali).
+ciudad(cartagena).
+ciudad(manizales).
+ciudad(barranquilla).
+ciudad(pasto).
+ciudad(monteria).
+
+% Hechos: vuelos directos
+vuelo(bogota, medellin).
+vuelo(medellin, cartagena).
+vuelo(cali, bogota).
+vuelo(bogota, cartagena).
+vuelo(manizales, cartagena).
+vuelo(medellin, barranquilla).
+vuelo(pasto, bogota).
+vuelo(bogota, pasto).
+
+escala(X,Y) :- vuelo(X,Z), vuelo(Z,Y).
+viaje(X,Y) :- escala(X,Y) ; vuelo(X, Y).
+destinos(X,Y) :- vuelo(X,Y).
